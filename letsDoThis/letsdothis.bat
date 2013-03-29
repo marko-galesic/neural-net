8b78f47edb43d111a56b5e43fa584a5a78495c5c
tavor <mf.galesic@gmail.com>
2013-03-29 17:00:57 -0400

    Scripts generated for testing.

---
+++ letsDoThis/letsdothis.bat
@@ -0,0 +1,7 @@
+@echo off
+python dieMaze.py Maze0.txt > test.txt 2>&1  
+python dieMaze.py Maze1.txt >> test.txt 2>&1
+python dieMaze.py Maze2.txt >> test.txt 2>&1
+python dieMaze.py Maze3.txt >> test.txt 2>&1
+python dieMaze.py Maze4.txt >> test.txt 2>&1
+python dieMaze.py Maze5.txt >> test.txt 2>&1
---
+++ letsDoThis/letsdothis.sh
@@ -0,0 +1,6 @@
+python dieMaze.py Maze0.txt > test.txt
+python dieMaze.py Maze1.txt >> test.txt
+python dieMaze.py Maze2.txt >> test.txt
+python dieMaze.py Maze3.txt >> test.txt
+python dieMaze.py Maze4.txt >> test.txt
+python dieMaze.py Maze5.txt >> test.txt
